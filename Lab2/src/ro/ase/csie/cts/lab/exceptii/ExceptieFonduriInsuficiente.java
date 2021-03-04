ba546c42ef8d7f78abd73d849667aca527c0b70f
Adriana <adrianabiza14@gmail.com>
2021-03-04 12:27:05 +0200

    lab2

---
+++ Lab2/src/ro/ase/csie/cts/lab/exceptii/ExceptieFonduriInsuficiente.java
@@ -0,0 +1,12 @@
+package ro.ase.csie.cts.lab.exceptii;
+
+public class ExceptieFonduriInsuficiente extends Exception {
+	
+	public ExceptieFonduriInsuficiente(String mesaj) {
+		super(mesaj);
+	}
+	
+	public ExceptieFonduriInsuficiente() {
+		
+	}
+}
