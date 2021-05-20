35589e3d0ffbb5f92da39fead7eb633756c85c42
Adriana <adrianabiza14@gmail.com>
2021-05-20 12:39:57 +0300

    "lab12 - pauza"

---
+++ Lab12 - Observer Memento/src/ro/ase/csie/cts/dp/memento/MementoSuperErou.java
@@ -0,0 +1,22 @@
+package ro.ase.csie.cts.dp.memento;
+
+import java.sql.Date;
+
+public class MementoSuperErou {
+
+	String nume;
+	int puncteViata;
+	String magie;
+	
+	Date dataSalvare;
+
+	public MementoSuperErou(String nume, int puncteViata, String magie) {
+		super();
+		this.nume = nume;
+		this.puncteViata = puncteViata;
+		this.magie = magie;
+
+	}
+	
+	
+}
